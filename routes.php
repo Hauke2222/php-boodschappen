88ec7295de5a207fc9e62a70ecbeea3f6f17bed6
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2020-07-24 11:30:35 +0200

    added . before route path

--- routes.php
+++ routes.php
@@ -1,9 +1,9 @@
 <?php
 
 $router->define([
-    '' => '/controllers/GroceriesController.php',
-    '/groceries' => '/controllers/GroceriesController.php',
-    'groceries/create' => 'controllers/GroceriesControllerCreateView.php',
+    '' => './controllers/GroceriesController.php',
+    '/groceries' => './controllers/GroceriesController.php',
+    'groceries/create' => './controllers/GroceriesControllerCreateView.php',
 ]);
 
 
